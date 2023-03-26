560d58f7d0af80aaba2bfd0672be10f0369ca78f
@faridiot <defaultride.bruno@gmail.com>
2023-03-26 16:33:13 +0700

    setItem(btnStorage, 'Hafal')

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -11,9 +11,9 @@ $(".panel-group_btn span").click(function(){
                        var btnStorage = $(this).attr("id");
 
                         if($(this).hasClass("clicked")) {
-                            localStorage.setItem(btnStorage, 'true');
+                            localStorage.setItem(btnStorage, 'Hafal');
                         } else {
-                            localStorage.removeItem(btnStorage, 'true');
+                            localStorage.removeItem(btnStorage, 'Hafal');
                         }
 
                     });
@@ -22,7 +22,7 @@ $(".panel-group_btn span").click(function(){
                     $( ".panel-group_btn span" ).each(function() {
                       var mainlocalStorage = $( this ).attr( "id" );
 
-                      if(localStorage.getItem(mainlocalStorage) == 'true') {
+                      if(localStorage.getItem(mainlocalStorage) == 'Hafal') {
                         $(this).addClass("clicked");
                     } else {
                         $(this).removeClass("clicked");
